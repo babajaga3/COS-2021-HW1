af3585464c87b605616cc325c15d227e3dbc0194
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-03-12 20:05:17 +0200

    :zap: Create new sorting algorithm design

--- structures/warehouse.cpp
+++ structures/warehouse.cpp
@@ -62,4 +62,55 @@ public:
             curr_shelf.print();
         }
     }
+
+    void sort_two() { // new sorting algorithm design for hw 2
+        // Step 1: check if there is an existing shelf?
+        // If no: create shelf, run function again
+        // If yes: continue
+
+        // Step 2: Check if sorting floor is empty?
+        // If yes: get crate from arrival queue, continue
+        // If no: get last element from sf, continue
+
+        // Step 3: Using element,
+        // Run function `can_put_on_shelf`?
+        // If yes: put and
+        // Run function again
+
+        // If no: If you remove last element, combine
+            //  new total weight with incoming element
+                // Is new total weight > old total weight && < max weight?
+                    // If yes:
+                    // remove last element and add to sorting floor
+
+                    /*
+                     *  check if you can put it on top
+                     *
+                     *  if no:
+                     *      remove last element to sorting floor
+                     *      try again
+                     *
+                     *  if yes:
+                     *      put to shelf
+                     *      run whole function recursively
+                     *
+                     */
+
+                    // If no:
+                        // remove the element before that last checked index - 1
+                        // try check again
+                        // if none work - go to next shelf (create new shelf and pass it in the function), and try whole function again.
+
+    }
+
+    void main_function() {
+
+    }
+
+    void can_put_on_shelf() {
+        // Check if you can add point-blank
+        // 1. number of elements
+        // 2. order
+        // 3. max weight
+    }
 };
